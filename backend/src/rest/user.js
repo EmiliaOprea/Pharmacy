911b9156834d9b47f1bbd7ebf16c9272d643dfef
Ioana Lazaroae <ioana_lazaroae@yahoo.com>
2017-12-13 16:53:36 +0200

    Update user.js

--- backend/src/rest/user.js
+++ backend/src/rest/user.js
@@ -3,7 +3,9 @@ var router = express.Router();
 var user = require('../service/').user;
 
 router.get('/', user.list);
-router.get('/:id', user.findById);
+router.get('/:id_client', user.findById);
 router.post('/', user.create);
+router.delete('/:id_client', user.delete);
+router.put('/:id_client',user.update);
 
 module.exports = router;

467f85888495956223428abb42d7b5d1b041cfaa
EmiliaOprea <31568425+EmiliaOprea@users.noreply.github.com>
2017-12-13 19:40:34 +0200

    Update user.js

--- backend/src/service/user.js
+++ backend/src/service/user.js
@@ -23,3 +23,45 @@ exports.findById = function (req, res) {
     res.jsonp(user);
   });
 };
+
+exports.delete = function (req, res) {
+  let id_client = req.params.id_client;
+  user.findById(req.params.id_client).then(user => {
+    if(!user){
+      return res.status(400).send({
+        message: 'User not found!',
+      });
+    }
+    return user
+      .destroy()
+      .then(() => res.status(204).send())
+      .catch(error => res.status(400).send(error));
+  })
+  .catch(error => res.status(400).send(error));
+};
+
+exports.update = function (req, res) {
+  let id_client = req.params.id_client;
+  user.findById(id_client).then(user =>{
+    if(!user){
+      return res.status(400).send({
+        message: 'Client Not Found',
+      });
+    }
+    
+    user.nume = req.body.name;
+    user.prenume = req.body.prenume;
+    user.varsta = req.body.varsta;
+    user.sex = req.body.sex;
+    user.createdAt = req.body.createdAt;
+    user.updateAt = req.body.updateAt;
+    
+    user.save(function(err) {
+      if (err)
+        res.send(err);
+      res.json({ message: 'Client was updated!'});
+    });
+  });
+};
+    
+

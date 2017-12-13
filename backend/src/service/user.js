ff72197ae22fac0e2027af8d283edb57b8b1febd
Ioana Lazaroae <ioana_lazaroae@yahoo.com>
2017-12-13 16:59:51 +0200

    Update user.js

--- backend/src/service/user.js
+++ backend/src/service/user.js
@@ -5,7 +5,7 @@ const user = require('../models').user;
 exports.list = function (req, res) {
   user.findAll().then(user => {
     res.jsonp(user);
-  });
+  }).catch((error) => rest.status(400).send(error));
 };
 
 exports.create = function (req, res) {
@@ -13,8 +13,13 @@ exports.create = function (req, res) {
 };
 
 exports.findById = function (req, res) {
-  let id = req.params.id;
-  user.findById(id).then(user => {
+  let id_client = req.params.id_client;
+  user.findById(id_client).then(user => {
+    if(!user){
+      return res.status(400).send({
+        message: 'User not found!',
+      });
+    }
     res.jsonp(user);
   });
 };
